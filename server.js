ba5521fc1c25e5c09f16b3d506245a9471739785
Colin Folta <colin.folta@gmail.com>
2020-03-25 10:34:57 -0500

    server notes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/hello', (req, res) => res.send({ message: 'Welcome to the Disinfection
 app.get('/distilleries', (req, res) => {
   const baseId = "app6GXpCRTsrba4vh"
   const table = "Distillery%20Hit%20List" //"Distilleries%20Form%20Results"
-  const numRecords = 1000;
+  const numRecords = 100;  // according to AirTable docs, if numRecords > pageSize, need to paginate. but pageSize max is 100.
   const view = "Master%20view"; //"Public%20View"
   const apiKey = process.env.AIRTABLE_API_KEY; // create a .env file with this var
 
@@ -29,11 +29,11 @@ app.get('/distilleries', (req, res) => {
   const newTimeStamp = (new Date()).getTime()
   if (distilleries.records.length > 0 && (newTimeStamp - lastFetched < 5 * 1000)) {
     console.log('sending cache')
-    return res.send({data: distilleries})
+    res.send({data: distilleries})
   } else {
     console.log('fetching')
     lastFetched = newTimeStamp
-    return fetch(`https://api.airtable.com/v0/${baseId}/${table}?maxRecords=${numRecords}&view=${view}`, {
+    fetch(`https://api.airtable.com/v0/${baseId}/${table}?maxRecords=${numRecords}&view=${view}`, {
       headers: {
         "Authorization": `Bearer ${apiKey}`
       }
@@ -41,7 +41,7 @@ app.get('/distilleries', (req, res) => {
     .then(res => res.json())
     .then(json => {
       distilleries = json
-      return res.send({data: distilleries})
+      res.send({data: distilleries})
     })
     .catch(err => res.err(err))
   }
